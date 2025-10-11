c61107a31c464b825b701a2fad296444444ad27c
Filip Madunicky <yelidric@gmail.com>
2025-10-12 01:29:42 +0200

    Adds functionality for blog post analysis
    Implements helper functions to analyze blog post data,
    including calculating total likes, finding the most liked
    post, and determining the author with the most posts.
    
    Also includes tests for these functions.

--- tests/list.test.js
+++ tests/list.test.js
@@ -2,9 +2,130 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+const emptyBlog = []
 
-  const result = listHelper.dummy(blogs)
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const blogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 3,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 8,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 8,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Alan',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 8,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 8,
+    __v: 0
+  }
+]
+
+test('dummy returns one', () => {
+  const result = listHelper.dummy(emptyBlog)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('of emty list is zero', () => {
+    const result = listHelper.totalLikes(emptyBlog)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 52)
+  })
+})
+
+describe('most favorite blog', () => {
+  test('if there is no posts', () => {
+    const result = listHelper.favoriteBlog(emptyBlog)
+    assert.strictEqual(result, undefined)
+  })
+
+  test('if there is one post', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.strictEqual(result, listWithOneBlog[0])
+  })
+
+  test('if there is many posts', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.strictEqual(result, blogs[1])
+  })
+})
+
+describe('most blogs by author', () => {
+  test('if there is no posts', () => {
+    const result = listHelper.mostBlogs(emptyBlog)
+    assert.strictEqual(result, undefined)
+  })
+
+  test('if there is one post', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', blogs: 1 })
+  })
+
+  test('if there is many posts', () => {
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, { author: 'Dijkstra', blogs: 3 })
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,42 @@
+const blog = require("../models/blog");
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  const likesCount = blogs.reduce((acumulator, post) => acumulator + post.likes, 0);
+  return likesCount
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined
+
+  const sortedBlogs = blogs.reduce((prevPost, post) => {
+    return post.likes > prevPost.likes ? post : prevPost
+  })
+  return sortedBlogs
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return undefined
+
+  const [author, blogCount] = lodash(blogs)
+    .countBy('author')
+    .toPairs()
+    .maxBy((pairs) => pairs[1])
+
+
+  return {
+    author,
+    blogs: blogCount
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }

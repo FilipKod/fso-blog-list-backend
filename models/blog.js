7b6025036f837952963e5dc92c43a5a32802c6d9
Filip Madunicky <yelidric@gmail.com>
2025-10-19 19:11:14 +0200

    Transforms blog post representation
    Ensures blog posts return "id" instead of "_id" as their unique identifier.
    
    This change modifies the `toJSON` transformation in the blog schema to map the `_id` field to `id` in the returned object. It also removes the `_id` and `__v` fields from the JSON representation to align with API best practices. A new test confirms the correct format.

--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
 
+/**
+ * @type {import('mongoose').Schema}
+ */
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.__v
+    delete returnedObject._id
+  }
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -13,17 +13,29 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initalPosts)
 })
 
-test.only('all posts are returned as json', async () => {
+test('all posts are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-type', /application\/json/)
 })
 
-test.only('all posts are returned', async () => {
+test('all posts are returned', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, helper.initalPosts.length)
+})
+
+test.only('blog posts identifier is returned as "id" not "_id"', async () => {
+  const response = await api.get('/api/blogs')
+
+  const firstPost = response.body[0]
+
+  assert.strictEqual(Object.hasOwn(firstPost, 'id'), true)
+
+  assert.strictEqual(Object.hasOwn(firstPost, '_id'), false)
+
+  assert.strictEqual(typeof firstPost.id, 'string')
 })
 
 after(async () => {

e0f34cb9d53b9d815c79a7095dc7b4a3f0f5e3e3
Filip Madunicky <yelidric@gmail.com>
2025-10-29 17:35:37 +0100

    Adds login functionality
    Adds a login route that authenticates users using bcrypt
    for password comparison and generates a JWT for
    authorization.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 const blogRouter = require('./controllers/blog')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const config = require('./utils/config')
 const errorHandler = require('./utils/middleware')
@@ -15,6 +16,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(errorHandler)
 
